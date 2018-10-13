50168b7f22c47c48be88e9fe11ea0b4295f614bf
Serhii Matrunchyk <serhii@digitalidea.studio>
2018-10-13 21:54:52 +0300

    Updated example according to the recent version of library

--- src/models/BaseModel.js
+++ src/models/BaseModel.js
@@ -1,9 +1,7 @@
 import { BaseModel as VGMBaseModel } from 'vue-graphql-models';
-import vue from '../main';
 
 export default class BaseModel extends VGMBaseModel {
   gqlLoader = path => import(/* webpackChunkName: "gql/[request]" */ `@/graphql/${path}.graphql`);
   dataLoader = path => import(/* webpackChunkName: "data/[request]" */ `@/data/${path}.json`);
   modelLoader = path => import(/* webpackChunkName: "models/[request]" */ `@/models/${path}`);
-  vue = vue;
 }
